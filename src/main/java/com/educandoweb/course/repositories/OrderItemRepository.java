8b4cb0a95d389349d435915e729bdaedbce07824
Destroier650 <alefemanuelmv@gmail.com>
2024-10-28 18:43:55 -0300

    Product-OrderItem one-to-many association

--- src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
+++ src/main/java/com/educandoweb/course/repositories/OrderItemRepository.java
@@ -1,10 +1,11 @@
 package com.educandoweb.course.repositories;
 
 import com.educandoweb.course.entities.Order;
+import com.educandoweb.course.entities.OrderItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface OrderRepository extends JpaRepository<Order, Long> {
+public interface OrderItemRepository extends JpaRepository<OrderItem, Long> {
 
 }
